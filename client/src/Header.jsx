5b95603746d2e1a6e078e0344baa93951bdfc070
Rehan Mondal <79845546+rehan13@users.noreply.github.com>
2024-09-17 22:59:52 +0530

    Name Changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 stroke-primary">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
         </svg>
-        <span className="text-primary font-bold text-3xl">smartbnb</span>
+        <span className="text-primary font-bold text-3xl">smartRentals</span>
         </Link>
         <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>

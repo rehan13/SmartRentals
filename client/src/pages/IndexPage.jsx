fddb16e6c21766bc85e09ad0937a6f718e526fc8
Rehan Mondal <79845546+rehan13@users.noreply.github.com>
2024-10-21 16:29:29 +0530

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -11,8 +11,7 @@ export default function IndexPage() {
   useEffect(() => {
     axios.get('/places').then(response => {
       setPlaces(response.data);
-      setFilteredPlaces(response.data);
-      console.log(response.data);
+      setFilteredPlaces(response.data); 
     });
   }, []);
 
@@ -28,30 +27,27 @@ export default function IndexPage() {
   }, [cityFilter, places]);
 
   return (
-    <>
-      <Header setCityFilter={setCityFilter} />
-      <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {filteredPlaces.length > 0 ? (
-          filteredPlaces.map(place => (
-            <Link to={'/place/' + place._id} key={place._id}>
-              <div>
-                <div className="bg-gray-500 mb-2 rounded-2xl flex">
-                  {place.photos?.[0] && (
-                    <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt="" />
-                  )}
-                </div>
-                <h2 className="font-bold">{place.address}</h2>
-                <h3 className="text-sm truncate leading-4">{place.title}</h3>
-                <div className="mt-1">
-                  <span>${place.price}</span> per night
-                </div>
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {filteredPlaces.length > 0 ? (
+        filteredPlaces.map(place => (
+          <Link to={'/place/' + place._id} key={place._id}>
+            <div>
+              <div className="bg-gray-500 mb-2 rounded-2xl flex">
+                {place.photos?.[0] && (
+                  <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt="" />
+                )}
               </div>
-            </Link>
-          ))
-        ) : (
-          <p>No places found for the entered city.</p>
-        )}
-      </div>
-    </>
+              <h2 className="font-bold">{place.address}</h2>
+              <h3 className="text-sm truncate leading-4">{place.title}</h3>
+              <div className="mt-1">
+                <span>${place.price}</span> per night
+              </div>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <p>No places found for the entered city.</p>
+      )}
+    </div>
   );
 }

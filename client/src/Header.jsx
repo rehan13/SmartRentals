18bdcaf263e755d51065f2e0075089540a0f239d
Rehan Mondal <79845546+rehan13@users.noreply.github.com>
2024-10-21 16:45:04 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -13,12 +13,12 @@ export default function Header({ setCityFilter }) {
   };
 
   return (
-    <header className="flex justify-between items-center px-4 py-2 md:px-8 md:py-4">
+    <header className="flex flex-col md:flex-row justify-between items-center px-4 py-2 md:px-8 md:py-4 space-y-4 md:space-y-0">
       <Link to={'/'} className="flex items-center gap-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 stroke-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 md:w-10 md:h-10 stroke-primary">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
         </svg>
-        <span className="text-primary font-bold text-3xl">smartRentals</span>
+        <span className="text-primary font-bold text-2xl md:text-3xl">smartRentals</span>
       </Link>
 
       <div className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300 transition-all duration-300 ease-in-out transform hover:scale-105 focus-within:scale-105 w-full md:w-1/3">
@@ -35,7 +35,6 @@ export default function Header({ setCityFilter }) {
           className="border-none outline-none text-sm w-full bg-transparent focus:outline-none focus:ring-0"
         />
       </div>
-
       <Link to={user ? '/account' : '/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -46,7 +45,7 @@ export default function Header({ setCityFilter }) {
           </svg>
         </div>
         {!!user && (
-          <div>{user.name}</div>
+          <div className="text-sm md:text-base">{user.name}</div>
         )}
       </Link>
     </header>
